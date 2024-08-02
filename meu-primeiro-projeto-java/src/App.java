528b542e67291feaa56d60cfc1617941de712214
PedroPadilhah <pedropadilhah@gmail.com>
2024-08-02 02:38:40 +0000

    meu primeiro projeto java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, mundo");
+        System.out.println("Bem vindo, Pedro");
     }
 }
